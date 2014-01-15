1685d83c13eed66eb2c13405d979207669d5bdbe
Neal Madlani <neal.madlani@guardian.co.uk>
2014-01-15 17:23:28 +0000

    updated check for social sign in on sign in page

--- signinpage.js
+++ signinpage.js
@@ -2,7 +2,7 @@
 casper.test.begin('Test: does sign in page exist', function(test) {
 	casper.start('https://profile.theguardian.com/signin', function() {
     	test.assertTitle('Sign in', 'expected: Sign in');
-	    test.assertVisible('.social-signin');
+	    test.assertVisible('.social-signin', 'expected to see social sign in buttons');
 	    // this.capture('signinpage.png');
 	});
 });
