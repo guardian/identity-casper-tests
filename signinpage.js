fc9529555942d27942d4578ae78932be3417f7d9
Neal Madlani <neal.madlani@guardian.co.uk>
2014-01-15 17:22:54 +0000

    Updated sico

--- signinpage.js
+++ signinpage.js
@@ -2,7 +2,7 @@
 casper.test.begin('Test: does sign in page exist', function(test) {
 	casper.start('https://profile.theguardian.com/signin', function() {
     	test.assertTitle('Sign in', 'expected: Sign in');
-	    test.assertVisible('social-signin');
+	    test.assertVisible('.social-signin');
 	    // this.capture('signinpage.png');
 	});
 });
